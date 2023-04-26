8a96156f4dfba8127d1eb3811afc66a947afdc7c
pablongo <paugallardocordon@gmail.com>
2023-04-26 21:32:40 +0200

    Remove footer unused links

--- next.config.js
+++ next.config.js
@@ -2,20 +2,24 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
+    domains: ['upcdn.io', 'replicate.delivery', 'lh3.googleusercontent.com']
+  },
+  i18n: {
+    locales: ['en', 'es', 'fr'],
+    defaultLocale: 'es'
   },
   async redirects() {
     return [
       {
-        source: "/github",
-        destination: "https://github.com/Nutlope/roomGPT",
-        permanent: false,
+        source: '/github',
+        destination: 'https://github.com/Nutlope/roomGPT',
+        permanent: false
       },
       {
-        source: "/deploy",
-        destination: "https://vercel.com/templates/next.js/room-GPT",
-        permanent: false,
-      },
+        source: '/deploy',
+        destination: 'https://vercel.com/templates/next.js/room-GPT',
+        permanent: false
+      }
     ];
-  },
+  }
 };
